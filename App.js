b1459d7c7c51220c0ae0a19feff042eca9990bc3
ChinChin2k5 <dgtienchien14@gmail.com>
2026-03-25 15:54:05 +0700

    Tập trung hoàn thiện module Admin, chính thức bàn giao lại luồng Login/Auth cho Duy phát triển độc lập

--- App.js
+++ App.js
@@ -1,75 +1,136 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { View ,StyleSheet, Text, ScrollView, Button, TouchableOpacity } from 'react-native';
+import { Users, FileText, Activity, Shield } from 'lucide-react-native';
+import { MaterialIcons} from '@expo/vector-icons';
 export default function App() {
+  const stats = [
+      { id: 1, title: 'Tổng người dùng', value: '1.234', sub: '156 giáo viên, 1078 học sinh',icon: <Users color="#10B981" size={24} /> },
+      { id: 2, title: 'Tổng đề thi', value: '856', sub: '48 đang hoạt động',icon: <FileText color="#10B981" size={24} />, },
+      { id: 3, title: 'Bài thi hôm nay', value: '342', sub: '128 đang diễn ra', icon: <Activity color="#A855F7" size={24} /> },
+      { id: 4, title: 'Hệ thống', value: '99.8%', sub: 'Hoạt động bình thường', icon: <Shield color="#F97316" size={24} /> },
+    ];
   return (
-    <View style={styles.mainScreen}>
-    <View style={styles.container}>
-      <Text style={styles.bigtitleText}>EduTest</Text>
-      <Text style={styles.subText}>Hệ thống đánh giá năng lực trực tuyến</Text>
-      <StatusBar style="auto" />
-    </View>
-    <View style={styles.card}>
-      <Text style={styles.titleText}>Chào Mừng Bạn</Text>
-      <Text style={styles.subText}>Đăng Nhập Hoặc Tạo Tài Khoản Để Bắt Đầu</Text>
-      <View style={styles.minicard}>
-        <TouchableOpacity style={styles.tabButtonActive}>
-          <Text style={styles.tabTextActive}>Đăng Nhập</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.tabButtonInActive}>
-          <Text style={styles.tabTextActive}>Đăng Ký</Text>
-        </TouchableOpacity>
-       </View>
-    </View>
-    </View>
-  );
+    //Tạo khung để tránh tràn sang tai thỏ
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {/*Tạo cuộn để lướt và xoá cái biểu tượng Scroll đi*/}
+        <ScrollView 
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollContent}>
+          <View style={[styles.header,styles.globalPadding, styles.globalBetween]}>
+            <View style={[styles.comeback,styles.globalBetween]}>
+              <MaterialIcons name="arrow-back" size={20} color="black" />
+              <Text style={styles.boldText}>Quay Lại</Text>
+            </View>
+            <View style={[styles.hello,styles.globalBetween]}>
+              <Text>Xin Chào</Text>
+              <Text style={styles.boldText}>Admin System</Text>
+              
+            </View>
+          </View>
+          <View style={[styles.body,styles.globalPadding]}>
+            <View>
+              <Text style={[styles.boldText,styles.dashBoard]}>Dashboard Quản Trị Viên</Text>
+              <Text style={styles.miniDashBoard}>Quản lý và giám sát toàn bộ hệ thống</Text>
+            </View>
+            <View>
+              <TouchableOpacity style={styles.buttonStyle}>
+                <Text style={styles.button}>⚙️ Cài Đặt Hệ Thống</Text>
+              </TouchableOpacity>
+            </View>
+            {stats.map((item) => (
+              <View key={item.id} style={styles.card}>
+                <View style={[{backgroundColor: item.color, justifyContent: 'center', alignItems: 'center' }]} />
+                {item.icon}
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.value}>{item.value}</Text>
+                <Text style={styles.subText}>{item.sub}</Text>
+              </View>
+            ))}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
 }
-
 const styles = StyleSheet.create({
-  mainScreen: {
-    flex: 1, 
-    backgroundColor: '#E0F7FA', // Màu nền xanh nhạt của app EduTest
-    alignItems: 'center',
-    justifyContent: 'center',
+  globalPadding: {
+    paddingHorizontal: 15,
+  },
+  globalBetween: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   container: {
-    marginBottom: 20,
+    flex: 1, 
+    backgroundColor: '#fff',
+  },
+  header: {
+    marginTop: 10,
+  },
+  body: {
+    marginTop: 30,
+    backgroundColor: '#E0E0E0',
+    padding: 20
+  },
+  comeback: {
+    gap: 8,
+  },
+  hello: {
+    gap: 8,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  dashBoard: {
+    fontSize: 24,
+  },
+  miniDashBoard: {
+    fontSize: 12,
+  },
+  button: {
+    fontSize: 18,
+    color: 'white',
+
+  },
+  buttonStyle: {
+    marginTop: 20,
     alignItems: 'center',
+    backgroundColor: '#007BFF',
+    borderRadius: 12,
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.35,
+    shadowRadius: 6,
+    elevation: 16,
   },
   card: {
-    backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '90%'
-  },
-  minicard: {
-    backgroundColor: 'gray',
-    padding: 10,
-    flexDirection: 'row',
     width: '100%',
-  },
-  titleText: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    marginBottom: 15,
-  },
-  subText: {
-    marginBottom: 20
-  },
-  bigtitleText: {
-    fontWeight: 'bold',
-    fontSize: 30,
-    marginBottom: 10,
-  },
-  tabButtonActive: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  tabButtonInActive: {
-    flex: 1,
+    backgroundColor: '#FFFFFF',
+    padding: 16,
+    marginTop: 20,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.05,
+    shadowRadius: 4,
+    elevation: 2,
     alignItems: 'center'
   },
+  title: {
+    fontSize: 14,
+    color: '#6B7280',
+    marginBottom: 4,
+  },
+  value: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#111827',
+    marginBottom: 4,
+  },
+  subText: {
+    fontSize: 12,
+    color: '#9CA3AF',
+  }
 });
-
